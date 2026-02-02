53d5eee0e503f4c16891869c771c10657cbf38b0
Sarthak <sarthakgarai05@gmail.com>
2026-02-02 23:02:30 +0530

    First Commit

---
+++ server.js
@@ -0,0 +1,16 @@
+const app = require('./app');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+        console.log("Connected to MongoDB");
+        app.listen(PORT, () => {
+            console.log(`Server is running on port ${PORT}`);
+        })
+    })
+    .catch((err) => {
+        console.error("Failed to connect to MongoDB", err);
+    })
