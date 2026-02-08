4f03b153e03044b7cc79d42cb67818eb14a70c61
Sarthak <sarthakgarai05@gmail.com>
2026-02-08 12:46:17 +0530

    feat(auth): initialize Express app and add signup route

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const authRoutes = require('./routes/authRoutes');
 
 const app = express();
 
@@ -9,5 +10,6 @@ app.use(express.json());
 app.get("/", (req, res) => {
     res.send("Express server is running");
 })
+app.use("/api/auth", authRoutes);
 
 module.exports = app;
