437c28438646bfd392616fe33bf2e57df9198c72
Sarthak <sarthakgarai05@gmail.com>
2026-02-09 23:57:56 +0530

    created sign in route

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,8 +1,9 @@
-const express = require('express');
-const { signup } = require('../controllers/authController');
+const express = require("express")
+const { signup, login } = require("../controllers/authController")
 
-const router = express.Router();
+const router = express.Router()
 
-router.post("/signup", signup);
+router.post("/signup", signup)
+router.post("/login", login)
 
-module.exports = router;
+module.exports = router
