d3fc7bddc436ba63dbdcdc3e835f2fac4f6227f0
Sarthak <sarthakgarai05@gmail.com>
2026-02-12 00:39:46 +0530

    feat: add refresh token route to auth routes

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,11 +1,12 @@
 const express = require("express")
-const { signup, login } = require("../controllers/authController")
+const { signup, login, refreshAccessToken } = require("../controllers/authController")
 const authMiddleware = require("../middleware/authMiddleware")
 
 const router = express.Router()
 
 router.post("/signup", signup)
 router.post("/login", login)
+router.post("/refresh", refreshAccessToken)
 
 //create a protected route for user profile 
 router.get("/profile", authMiddleware , (req, res) => {
