03be2255b32c650a4ff438f677a2040318e12ab3
Sarthak <sarthakgarai05@gmail.com>
2026-02-11 23:17:54 +0530

    feat(auth): implement JWT token validation middleware

---
+++ middleware/authMiddleware.js
@@ -0,0 +1,26 @@
+const jwt = require('jsonwebtoken');
+
+const authMiddleware = (req, res, next) => {
+
+    try {
+        const authHeader = req.headers.authorization
+
+        if (!authHeader || !authHeader.startsWith("Bearer ")) {
+            return res.status(401).json({ message: "Access Denied!!! Invalid token" })
+        }
+    
+
+const token = authHeader.split(" ")[1]
+
+    const decoded = jwt.verify(token, process.env.JWT_ACCESS_SECRET)
+
+    req.user = decoded
+
+    next()
+
+}catch (error) {
+    return res.status(401).json({ message: "Invalid token " })
+}
+}
+
+module.exports = authMiddleware
