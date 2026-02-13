ebe6a92c8293a6a66552803c91ef0be4da5711d2
Sarthak <sarthakgarai05@gmail.com>
2026-02-14 00:48:17 +0530

    feat(auth): add logout route

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,5 +1,5 @@
 const express = require("express")
-const { signup, login, refreshAccessToken } = require("../controllers/authController")
+const { signup, login, refreshAccessToken ,logout } = require("../controllers/authController")
 const authMiddleware = require("../middleware/authMiddleware")
 
 const router = express.Router()
@@ -7,6 +7,7 @@ const router = express.Router()
 router.post("/signup", signup)
 router.post("/login", login)
 router.post("/refresh", refreshAccessToken)
+router.post("/logout",logout)
 
 // create a protected route for user profile
 router.get("/profile", authMiddleware, (req, res) => {
