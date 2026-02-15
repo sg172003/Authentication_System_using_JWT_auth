34f682c94012d70e5bbdfe3eeddcc710a6c9ecd8
Sarthak <sarthakgarai05@gmail.com>
2026-02-15 14:13:07 +0530

    feat(frontend): implement protected route for profile page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route, Navigate } from "react-router-dom"
 import Signup from "./pages/Signup"
 import Login from "./pages/Login"
 import Profile from "./pages/Profile"
+import ProtectedRoute from "./components/ProtectedRoute"
 
 function App() {
   return (
@@ -9,7 +10,14 @@ function App() {
       <Route path="/" element={<Navigate to="/login" />} />
       <Route path="/signup" element={<Signup />} />
       <Route path="/login" element={<Login />} />
-      <Route path="/profile" element={<Profile />} />
+      <Route
+        path="/profile"
+        element={
+          <ProtectedRoute>
+            <Profile />
+          </ProtectedRoute>
+        }
+      />
     </Routes>
   )
 }
